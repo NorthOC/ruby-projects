0ba1fb2ba34f9e510f6eea0e39e6514e6634a7be
Denis Lisunov <rev.denisas@gmail.com>
2021-08-20 19:35:52 +0300

    added comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,12 +1,23 @@
 def stock_picker(arr)
+
 #defining needed variables
+
     profit = 0
     stock_day = []
     sell_day = 0
     buy_day = 0
+
+#looping through the buy and sell
+
     arr.each do |buy|
         arr.each do |sell|
+
+#The buy day should always be before the sell day and we're looking for maximum profit
+
             if (arr.index(buy) < arr.index(sell) and sell - buy > profit)
+
+#saving our findings into variables each time conditions are met
+
                 sell_day = arr.index(sell)
                 buy_day = arr.index(buy)
                 profit = sell - buy
@@ -14,9 +25,14 @@ def stock_picker(arr)
             end
         end
     end
+
+#output
+
     puts "Best day to buy: #{buy_day}"
     puts "Best day to sell is day #{sell_day}"
     puts "Max profit: #{profit}"
     return stock_day
 end
-print stock_picker([17,3,6,9,15,8,6,1,10])
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+# => [1,4]
